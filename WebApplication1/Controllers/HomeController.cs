ad0741ab5dac3b808b1072b1d1e526111b8cbe4c
Ray Cerdena <ray.cerdena@gmail.com>
2018-05-19 14:51:04 +0800

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private ApplicationDbContext _context;
+        //public HomeController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+
+
+        public ActionResult Register()
+        {
+
+
+
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Helper/ValidateAntiForgeryToken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace WebApplication1.Helper
+{
+    public class ValidateAntiForgeryToken : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            var headerToken = actionContext
+                .Request
+                .Headers
+                .GetValues("__RequestVerificationToken")
+                .FirstOrDefault();
+
+            var cookieToken = actionContext
+                .Request
+                .Headers
+                .GetCookies()
+                .Select(c => c[AntiForgeryConfig.CookieName])
+                .FirstOrDefault();
+
+            // check for missing cookie or header
+            if (cookieToken == null || headerToken == null)
+            {
+                return false;
+            }
+
+            // ensure that the cookie matches the header
+            try
+            {
+                AntiForgery.Validate(cookieToken.Value, headerToken);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return base.IsAuthorized(actionContext);
+        }
+
+
+    }
+}
